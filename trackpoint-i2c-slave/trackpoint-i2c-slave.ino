3261242b3d88f8e0123cfbb6a06045eabf801965
Magid William <Maged.Ewilliam@gmail.com>
2026-07-26 00:34:10 +0300

    Exp19: TTP223 data gate (no drift at rest) + revert burst signs

--- trackpoint-i2c-slave/trackpoint-i2c-slave.ino
+++ trackpoint-i2c-slave/trackpoint-i2c-slave.ino
@@ -110,23 +110,26 @@ void loop() {
     int8_t x, y;
     uint8_t buttons;
 
-    if (ps2.readPacket(x, y, buttons)) {
-        idle_start = 0;
-        boot_grace = false;
-        if (abs(x) >= 127 || abs(y) >= 127) {
-        } else if (wake_discard) {
-            wake_discard--;
-        } else {
-            x = -x;
-            y = -y;
-            if (abs(x) < 3 && abs(y) < 3) {
-                x = 0; y = 0;
+    if (digitalRead(TOUCH_PIN) == HIGH) {
+        if (ps2.readPacket(x, y, buttons)) {
+            idle_start = 0;
+            boot_grace = false;
+            if (abs(x) >= 127 || abs(y) >= 127) {
+            } else if (wake_discard) {
+                wake_discard--;
+            } else {
+                x = -x;
+                y = -y;
+                if (abs(x) < 3 && abs(y) < 3) {
+                    x = 0; y = 0;
+                }
+                burst_x = y;
+                burst_y = x;
             }
-            burst_x = -y;
-            burst_y = -x;
-            Serial.print("X:"); Serial.print(burst_x);
-            Serial.print(" Y:"); Serial.println(burst_y);
         }
+    } else {
+        burst_x = 0;
+        burst_y = 0;
     }
 
     if (!pulsed && (millis() - last_mot >= MOT_PERIOD_MS)) {
