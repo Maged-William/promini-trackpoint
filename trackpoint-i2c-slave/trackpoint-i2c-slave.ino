c8e1a394a73ad3ca16508336b8a94847b66f4930
Magid William <Maged.Ewilliam@gmail.com>
2026-07-26 19:33:16 +0300

    Exp20: ditch TTP223, use CLK-inhibit + WDT wake

--- trackpoint-i2c-slave/trackpoint-i2c-slave.ino
+++ trackpoint-i2c-slave/trackpoint-i2c-slave.ino
@@ -4,7 +4,6 @@
 
 #define I2C_ADDR     0x42
 #define MOT_PIN      14
-#define TOUCH_PIN    2
 #define NPN_PIN      4
 #define PMOS_PIN     6
 #define LED_PIN      13
@@ -28,6 +27,7 @@ static int8_t        calib_x = 0, calib_y = 0;
 static int32_t       calib_sum_x = 0, calib_sum_y = 0;
 static uint16_t      calib_count = 0;
 static unsigned long calib_end_ms = 0;
+static unsigned long last_motion_ms = 0;
 
 
 void requestEvent() {
@@ -43,8 +43,6 @@ void receiveEvent(int len) {
     if (len > 0) cur_addr = Wire.read();
 }
 
-static void wakeUp() {}
-
 static void pulse_mot() {
     digitalWrite(MOT_PIN, LOW);
     delayMicroseconds(100);
@@ -56,8 +54,6 @@ void setup() {
     digitalWrite(NPN_PIN, HIGH);
     pinMode(PMOS_PIN, OUTPUT);
     digitalWrite(PMOS_PIN, LOW);
-    pinMode(TOUCH_PIN, INPUT_PULLUP);
-
     pinMode(MOT_PIN, OUTPUT);
     digitalWrite(MOT_PIN, HIGH);
 
@@ -68,7 +64,9 @@ void setup() {
     ps2.begin();
 
     Serial.begin(115200);
-    Serial.println("--- I2C Slave + PS/2 + Sleep ---");
+    Serial.println("--- I2C Slave + PS/2 + CLK-inhibit WDT Sleep ---");
+
+    last_motion_ms = millis();
 }
 
 void loop() {
@@ -79,13 +77,82 @@ void loop() {
     int8_t x, y;
     uint8_t buttons;
 
-    if (digitalRead(TOUCH_PIN) == HIGH) {
-        unsigned long now = millis();
-        if (now - last_read_ms >= READ_INTERVAL_MS) {
-            last_read_ms = now;
-            if (ps2.readPacket(x, y, buttons)) {
-                last_ps2_ms = millis();
+    unsigned long now = millis();
+    if (now - last_read_ms >= READ_INTERVAL_MS) {
+        last_read_ms = now;
+        if (ps2.readPacket(x, y, buttons)) {
+            last_ps2_ms = millis();
 
+            if (!calibrated) {
+                if (millis() >= calib_end_ms) {
+                    if (calib_count > 0) {
+                        calib_x = (int8_t)(calib_sum_x / calib_count);
+                        calib_y = (int8_t)(calib_sum_y / calib_count);
+                    }
+                    calibrated = true;
+                } else if (abs(x) < 127 && abs(y) < 127) {
+                    calib_sum_x += x;
+                    calib_sum_y += y;
+                    calib_count++;
+                } else {
+                    burst_x = 0; burst_y = 0;
+                }
+            }
+
+            if (calibrated) {
+                if (abs(x) >= 127 || abs(y) >= 127 || abs(x) > MAX_DELTA || abs(y) > MAX_DELTA) {
+                    burst_x = 0; burst_y = 0;
+                } else {
+                    int32_t cx = (int32_t)x - calib_x;
+                    int32_t cy = (int32_t)y - calib_y;
+
+                    x = (cx < -128) ? -128 : (cx > 127) ? 127 : (int8_t)cx;
+                    y = (cy < -128) ? -128 : (cy > 127) ? 127 : (int8_t)cy;
+
+                    burst_x = x;
+                    burst_y = y;
+                    if (burst_x || burst_y) {
+                        Serial.print(burst_x);
+                        Serial.print(",");
+                        Serial.println(burst_y);
+                        was_moving = 1;
+                        pulse_mot();
+                        last_motion_ms = millis();
+                    }
+                }
+            }
+        }
+    }
+
+    if (burst_x || burst_y) {
+        if (millis() - last_ps2_ms > 100) {
+            burst_x = 0;
+            burst_y = 0;
+            if (was_moving) {
+                Serial.println("0");
+                was_moving = 0;
+            }
+        }
+    }
+
+    if (millis() - last_motion_ms >= IDLE_TIMEOUT_MS) {
+        Serial.println("Sleeping...");
+        Serial.flush();
+
+        pinMode(PS2_CLK, OUTPUT);
+        digitalWrite(PS2_CLK, LOW);
+        TWCR = 0;
+
+        while (1) {
+            LowPower.powerDown(SLEEP_60MS, ADC_OFF, BOD_OFF);
+
+            Wire.begin(I2C_ADDR);
+            Wire.onRequest(requestEvent);
+            Wire.onReceive(receiveEvent);
+
+            pinMode(PS2_CLK, INPUT_PULLUP);
+
+            if (ps2.readPacket(x, y, buttons)) {
                 if (!calibrated) {
                     if (millis() >= calib_end_ms) {
                         if (calib_count > 0) {
@@ -97,71 +164,36 @@ void loop() {
                         calib_sum_x += x;
                         calib_sum_y += y;
                         calib_count++;
-                    } else {
-                        burst_x = 0; burst_y = 0;
                     }
                 }
 
                 if (calibrated) {
-                    if (abs(x) >= 127 || abs(y) >= 127 || abs(x) > MAX_DELTA || abs(y) > MAX_DELTA) {
-                        burst_x = 0; burst_y = 0;
-                    } else {
+                    if (!(abs(x) >= 127 || abs(y) >= 127 || abs(x) > MAX_DELTA || abs(y) > MAX_DELTA)) {
                         int32_t cx = (int32_t)x - calib_x;
                         int32_t cy = (int32_t)y - calib_y;
-
                         x = (cx < -128) ? -128 : (cx > 127) ? 127 : (int8_t)cx;
                         y = (cy < -128) ? -128 : (cy > 127) ? 127 : (int8_t)cy;
-
-                        burst_x = x;
-                        burst_y = y;
-                        if (burst_x || burst_y) {
+                        if (x || y) {
+                            burst_x = x;
+                            burst_y = y;
+                            Serial.print("W:");
                             Serial.print(burst_x);
                             Serial.print(",");
                             Serial.println(burst_y);
                             was_moving = 1;
                             pulse_mot();
+                            last_motion_ms = millis();
+                            last_ps2_ms = millis();
+                            Serial.println("Woke!");
+                            break;
                         }
                     }
                 }
             }
-        }
-        if (burst_x || burst_y) {
-            if (millis() - last_ps2_ms > 100) {
-                burst_x = 0;
-                burst_y = 0;
-                if (was_moving) {
-                    Serial.println("0");
-                    was_moving = 0;
-                }
-            }
-        }
-    } else {
-        burst_x = 0;
-        burst_y = 0;
-        if (was_moving) {
-            Serial.println("0");
-            was_moving = 0;
-        }
-    }
 
-    static unsigned long idle_start = 0;
-    if (digitalRead(TOUCH_PIN) == LOW) {
-        if (idle_start == 0) {
-            idle_start = millis();
-        } else if (millis() - idle_start >= IDLE_TIMEOUT_MS) {
-            idle_start = 0;
-            Serial.println("Sleeping...");
-            Serial.flush();
             TWCR = 0;
-            attachInterrupt(digitalPinToInterrupt(TOUCH_PIN), wakeUp, RISING);
-            LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
-            detachInterrupt(digitalPinToInterrupt(TOUCH_PIN));
-            Wire.begin(I2C_ADDR);
-            Wire.onRequest(requestEvent);
-            Wire.onReceive(receiveEvent);
-            Serial.println("Woke!");
+            pinMode(PS2_CLK, OUTPUT);
+            digitalWrite(PS2_CLK, LOW);
         }
-    } else {
-        idle_start = 0;
     }
 }
